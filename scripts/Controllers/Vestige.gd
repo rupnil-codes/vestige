d34aa55f7c5e8dbe3a73b12720ab1f3e42797c30
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-08-22 23:16:51 +0530

    (v0.5.2-beta) Vestiges 3,4,5 Animation
    I thought i would complete making the animations today but clearly I failed- Oh well I fixed a LOT of things and FULY completete the third forth and fifth vestiges

--- scripts/Controllers/Vestige.gd
+++ scripts/Controllers/Vestige.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var vestige_animation_player: AnimationPlayer = $VestigeAnimationPlayer
+@onready var vestige_once_animation_player: AnimationPlayer = $VestigeOnceAnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +16,4 @@ func _process(delta: float) -> void:
 func _on_vestige_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player3D":
 		vestige_animation_player.play("vestige_animation")
+		vestige_once_animation_player.play("play_once")
