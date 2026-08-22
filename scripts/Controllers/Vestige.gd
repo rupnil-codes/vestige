74d3d780897d1678ec5a7cc50f2913bbb712ffb5
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-08-22 20:26:09 +0530

    (v0.5.1-beta) Vestiges 1,2,3 Animation
    Since I completed modelling and texturing the vestiges the only logical next step is to incorporate them into the game, and thats what ive been doing! Integrated the first 3 vestiges and added some simple animations associated w em

---
+++ scripts/Controllers/Vestige.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@onready var vestige_animation_player: AnimationPlayer = $VestigeAnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_vestige_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == "Player3D":
+		vestige_animation_player.play("vestige_animation")
